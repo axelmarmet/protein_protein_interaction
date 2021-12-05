faf0578230dc30f46be4f9676e2dc52668412ee9
Loïc BUSSON <loic.busson@epfl.ch>
2021-12-05 19:02:41 +0100

    adding all_gather using pickle

--- src/ppi_pred/utils.py
+++ src/ppi_pred/utils.py
@@ -8,6 +8,7 @@ from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, r
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pickle
 
 def set_seed(seed=44):
     random.seed(seed)
@@ -18,7 +19,6 @@ def set_seed(seed=44):
 
 
 # necessary functions for distributed training
-
 def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12355'
@@ -29,6 +29,59 @@ def setup(rank, world_size):
 def cleanup():
     dist.destroy_process_group()
 
+def get_world_size():
+    if not dist.is_available():
+        return 1
+    if not dist.is_initialized():
+        return 1
+    return dist.get_world_size()
+
+#All gather from https://github.com/facebookresearch/maskrcnn-benchmark/blob/main/maskrcnn_benchmark/utils/comm.py making use of picklable data
+def all_gather(data):
+    """
+    Run all_gather on arbitrary picklable data (not necessarily tensors)
+    Args:
+        data: any picklable object
+    Returns:
+        tensor of data gathered from each rank
+    """
+    world_size = get_world_size()
+    if world_size == 1:
+        return [data]
+
+    # serialized to a Tensor
+    buffer = pickle.dumps(data)
+    storage = torch.ByteStorage.from_buffer(buffer)
+    tensor = torch.ByteTensor(storage).to("cuda")
+
+    # obtain Tensor size of each rank
+    local_size = torch.LongTensor([tensor.numel()]).to("cuda")
+    size_list = [torch.LongTensor([0]).to("cuda") for _ in range(world_size)]
+    dist.all_gather(size_list, local_size)
+    size_list = [int(size.item()) for size in size_list]
+    max_size = max(size_list)
+
+    # receiving Tensor from all ranks
+    # we pad the tensor because torch all_gather does not support
+    # gathering tensors of different shapes
+    tensor_list = []
+    for _ in size_list:
+        tensor_list.append(torch.ByteTensor(size=(max_size,)).to("cuda"))
+    if local_size != max_size:
+        padding = torch.ByteTensor(size=(max_size - local_size,)).to("cuda")
+        tensor = torch.cat((tensor, padding), dim=0)
+    dist.all_gather(tensor_list, tensor)
+
+    #I modified a bit the end of the code to match our output format
+    #data_list = []
+    data_tensor = torch.tensor([])
+    for size, tensor in zip(size_list, tensor_list):
+        buffer = tensor.cpu().numpy().tobytes()[:size]
+        #data_list.append(pickle.loads(buffer))
+        data_tensor = torch.cat(data_tensor,pickle.loads(buffer))
+
+    #return data_list
+    return data_tensor
 
 def plot_confusion_matrix(CM):
     
